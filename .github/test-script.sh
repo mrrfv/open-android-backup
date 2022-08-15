75fa2c6063a09f41abd5d59d09ef1bcb20fc2ae0
mrrfv <rm-rfv-no-preserve-root@protonmail.com>
2022-08-15 21:47:15 +0200

    WIP: automatic testing on GitHub Actions

---
+++ .github/test-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+adb shell mkdir -p /storage/emulated/0/linux-android-backup-temp
+
+export unattended_mode="yes"
+export selected_action="Backup"
+export archive_path="/tmp"
+export archive_password="123"
+
+./backup.sh
