5fa5eb2fcb9225349c5db66c22529dd336800ee8
mrrfv <rm-rfv-no-preserve-root@protonmail.com>
2023-01-28 20:56:21 +0100

    update testing for macOS

--- .github/test-script.sh
+++ .github/test-script.sh
@@ -6,6 +6,14 @@ export unattended_mode="yes"
 export selected_action="Backup"
 export archive_path="/tmp"
 export archive_password="123"
+export mode="Wired"
+export export_method="adb"
+export use_hooks="no"
+export TERM="nope"
 export CI="true"
 
-./backup.sh
+faketty() {
+    script -qfc "$(printf "%q " "$@")" /dev/null
+}
+
+faketty ./backup.sh
