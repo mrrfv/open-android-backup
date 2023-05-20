2cc4306b8faec3860ff57cbefd1027a77b32f2b6
mrrfv <rm-rfv-no-preserve-root@protonmail.com>
2023-05-20 15:11:39 +0200

    Update test-script.sh

--- .github/test-script.sh
+++ .github/test-script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-adb shell mkdir -p /storage/emulated/0/open-android-backup-temp
-adb shell touch /storage/emulated/0/open-android-backup-temp/SMS_Messages.csv
+create_dummy_files() {
+    adb shell mkdir -p /storage/emulated/0/open-android-backup-temp
+    adb shell touch /storage/emulated/0/open-android-backup-temp/SMS_Messages.csv
+}
 
 export unattended_mode="yes"
 export archive_path="/tmp"
@@ -14,24 +16,28 @@ export data_erase_choice="Fast"
 # Backup
 export selected_action="Backup"
 
-# Hooks disabled,  ADB exporting method
+# Hooks disabled, ADB exporting method
 export use_hooks="no"
 export export_method="adb"
+create_dummy_files
 ./backup.sh
 
 # Hooks disabled, TAR exporting method
 export use_hooks="no"
 export export_method="tar"
+create_dummy_files
 ./backup.sh
 
 # Hooks enabled, ADB exporting method
 export use_hooks="yes"
 export export_method="adb"
 cp .github/test-hook.sh ./hooks.sh
+create_dummy_files
 ./backup.sh
 
 # Hooks enabled, TAR exporting method
 export use_hooks="yes"
 export export_method="tar"
 cp .github/test-hook.sh ./hooks.sh
+create_dummy_files
 ./backup.sh
