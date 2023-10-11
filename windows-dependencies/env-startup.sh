9c3ef52ba59edb35663391e57024b8b6feec7cac
mrrfv <rm-rfv-no-preserve-root@protonmail.com>
2023-10-11 16:28:20 +0200

    Explain why Windows adb.exe is used in WSL

--- windows-dependencies/env-startup.sh
+++ windows-dependencies/env-startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# we're using the Windows adb
+# we're using the Windows adb because WSL doesn't have direct access to the Android device
+# (if we were to use the Linux adb, no devices would be detected)
 export ADB_DIR=$(pwd)/windows-dependencies/adb/adb.exe
 
 adb() {
