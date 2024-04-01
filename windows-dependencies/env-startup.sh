d7e52826abd69d9ee1488805aebbbd235440e354
mrrfv <rm-rfv-no-preserve-root@protonmail.com>
2024-04-01 12:35:17 +0200

    Explain env-startup.sh with greater clarity

--- windows-dependencies/env-startup.sh
+++ windows-dependencies/env-startup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# we're using the Windows adb because WSL doesn't have direct access to the Android device
-# (if we were to use the Linux adb, no devices would be detected)
+# This script is a workaround for the fact that WSL doesn't have direct access to Android devices.
+# We're using the Windows adb executable to interact with the device, which runs on the Windows host.
+# If we were to use the Linux adb, no devices would be detected.
+
 export ADB_DIR=$(pwd)/windows-dependencies/adb/adb.exe
 
 adb() {
