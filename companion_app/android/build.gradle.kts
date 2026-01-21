0b5b06a5c9f7c6e5a19aeae5dc6bf5b5027ecdd0
mrrfv <rm-rfv-no-preserve-root@protonmail.com>
2026-01-21 16:14:44 +0100

    app: Bump Android compile SDK to 36 and Java to 21
    Fixes build problems.

---
+++ companion_app/android/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    id("com.android.application") version "8.9.1" apply false
+    id("com.android.library") version "8.9.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
